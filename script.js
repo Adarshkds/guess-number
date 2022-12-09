936c423f116aca9c9a5f4ee9bbedb85c94c5857a
Adarsh Sinh <adarshkds8@gmail.com>
2022-12-09 15:11:43 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 'use strict';
 
-// Starters
+// lets;
 let score = 20;
 let highScore = 0;
 let secretNum = Math.floor(Math.random() * 20) + 1;
+console.log(secretNum);
 
 const message = document.querySelector(`.message`);
 
+// code
 document.querySelector(`.check`).addEventListener(`click`, function () {
-  const guess = document.querySelector(`guess`).value;
+  const guess = document.querySelector(`.guess`).value;
   if (!guess) {
-    document.querySelector(`.message`).textContent = `X No Input X`;
+    message.textContent = `X No Input X`;
   } else if (guess == secretNum) {
     message.textContent = `You Won`;
+    document.querySelector(`.number`).textContent = secretNum;
+    document.querySelector(`body`).style.backgroundColor = `#60b347`;
+    document.querySelector(`.number`).style.width = `30rem`;
+  } else if (guess > secretNum) {
+    message.textContent = `Too high`;
+    document.querySelector(`.score`).textContent = score--;
+  } else if (guess < secretNum) {
+    message.textContent = `Too low`;
   }
 });
